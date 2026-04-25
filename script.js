09da08080f5d46d9c341423a5617f0b2298dd746
Laura Neira <lauraneira62@gmail.com>
2026-04-24 22:54:33 -0500

    Corrección final para entrega

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-
-
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir-menu");
-const cerrar = document.querySelector("#cerrar-menu");
-
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
-
-
+/* Con esto hacemos que el menú hamburguesa sea visible solo a partir del tamaño de ciertas pantallas y se oculte en pantallas más grandes. */ 
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir-menu");
 const cerrar = document.querySelector("#cerrar-menu");
